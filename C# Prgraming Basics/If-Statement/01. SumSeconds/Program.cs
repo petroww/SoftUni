acb843363056808513ca29bcdf46a9b340c0aacf
petroww <miroslavpetroww@gmail.com>
2021-10-17 21:42:02 +0300

    add exercises from if-statement

---
+++ C# Prgraming Basics/If-Statement/01. SumSeconds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Coditional_Statemant_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int suming = first + second + third;
+
+            int minutes = suming / 60;
+            int secounds = suming % 60;
+
+            if (secounds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{secounds}");
+            }
+            else
+                Console.WriteLine($"{minutes}:{secounds}");
+        }
+    }
+}
